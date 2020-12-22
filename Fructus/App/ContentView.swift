c4b0405235baa40cc0de5319fb88da54d96e8a30
Clydies Freeman <clyde@joinsoko.com>
2020-12-22 00:29:10 -0600

    Initial Commit

---
+++ Fructus/App/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by clydies freeman on 12/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // MARK: - PROPERTIES
+    var fruits: [Fruit] = fruitsData
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(fruits.shuffled()) { item in
+                    FruitRowView(fruit: item)
+                        .padding(.vertical, 4)
+                }
+            }
+            .navigationTitle("Fruits")
+        } //: NAVIGATION
+    }
+}
+
+// MARK: - PREVIEW
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fructus/View/StartButton.swift
@@ -0,0 +1,42 @@
+//
+//  StartButton.swift
+//  Fructus
+//
+//  Created by clydies freeman on 12/21/20.
+//
+
+import SwiftUI
+
+struct StartButton: View {
+    // MARK: - PROPERTIES
+    @AppStorage("isOnboarding") var isOnboarding: Bool?
+    
+    // MARK: - BODY
+    var body: some View {
+        Button(action: {
+        isOnboarding = false
+        }, label: {
+            HStack(spacing: 8) {
+                Text("Start")
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(
+                Capsule().strokeBorder(Color.white, lineWidth: 1.25)
+            )
+        }) // : BUTTON
+        .accentColor(.white)
+    }
+}
+
+// MARK: - BODY
+
+struct StartButton_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButton()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+    }
+}
