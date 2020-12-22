2b28014b8d34d0efaa7e942bd7bacf7a6423a855
Clydies Freeman <clyde@joinsoko.com>
2020-12-21 22:01:55 -0600

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by clydies freeman on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
